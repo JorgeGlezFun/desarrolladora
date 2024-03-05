e06162fd72db72719af663a381578991d74b120a
Jorge Gonzalez Fuentes <jorgegonfue4d@gmail.com>
2024-03-05 14:53:40 +0100

    Comentarios en web.php

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,11 @@ Route::view('profile', 'profile')
 
 require __DIR__.'/auth.php';
 
+// El route:get nos devuelve una vista que responde a la URL /videojuegos/poseo,
+// donde haciendo uso del controlador de Videojuego nos devuelve la funcion poseo()
+// con name('videojuegos.poseo') facilitamos la referencia en otras partes del codigo
 Route::get('/videojuegos/poseo', [VideojuegoController::class, 'poseo'])->name('videojuegos.poseo');
+// Lo mismo pero con una ruta post
 Route::post('/videojuegos/poseo', [VideojuegoController::class, 'poseoUpdate'])->name('videojuegos.poseoUpdate');
 // El middleware('auth') te manda a loguearte pasando antes por ahi que por la pagina a la que quieres ir
 Route::resource('videojuegos', VideojuegoController::class)->middleware('auth');
